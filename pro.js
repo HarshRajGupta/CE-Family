68ddf56ba5f3ff82a26200826f664002ea99f92d
SIMRON BHUYAN <simron0707@gmail.com>
2021-09-16 23:10:13 +0530

    Add files via upload

---
+++ pro.js
@@ -0,0 +1,61 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get("/welcome", function(req, res) {
+    res.sendFile(__dirname + "/loading.html");
+});
+
+app.get("/forgotPassword", function(req, res) {
+    res.sendFile(__dirname + "/register.html");
+});
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/login.html");
+});
+app.post("/forgotPassword",function(req, res){
+    res.send("We will contact you as soon as possible")
+});
+app.post("/", function(req, res) {
+	var value = req.body.button;
+    if (value === "login") 
+    {
+        var username = req.body.username;
+        var password = req.body.password;
+        if (password === username && username.substr(0, 5) === "b5200") 
+        {
+            res.sendFile(__dirname + "/Files/home.html");
+        } 
+        else 
+        {
+            res.sendFile(__dirname + "/error.html");
+        }
+    }
+    else if (value === "home")
+    {
+    	res.sendFile(__dirname + "/Files/home.html");
+    }
+    else if (value === "timeTable")
+    {
+    	res.sendFile(__dirname + "/Files/time.html");
+    }
+    else if (value === "notice")
+    {
+    	res.sendFile(__dirname + "/Files/notice.html");
+    }
+    else if (value === "Logout")
+    {
+    	res.sendFile(__dirname + "/Files/sucess.html");
+    }
+});
+
+app.get("#", function(req, res) 
+{
+    res.sendFile(__dirname + "/loading.html")
+})
+
+app.listen(7007, function() {
+    console.log("Server is running on port 7007")
+});
